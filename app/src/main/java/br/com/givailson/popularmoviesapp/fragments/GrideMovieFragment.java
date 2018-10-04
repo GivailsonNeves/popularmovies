59d55666b853e6a7f4afbed63ee02de4349c087b
Givailson Neves <givailsonneves@wizsolucoes.com.br>
2018-10-04 19:12:59 -0300

    listagem da gridview

---
+++ app/src/main/java/br/com/givailson/popularmoviesapp/fragments/GrideMovieFragment.java
@@ -0,0 +1,62 @@
+package br.com.givailson.popularmoviesapp.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import br.com.givailson.popularmoviesapp.R;
+import br.com.givailson.popularmoviesapp.adapters.MovieGridAdapter;
+import br.com.givailson.popularmoviesapp.models.Movie;
+import br.com.givailson.popularmoviesapp.services.MovieService;
+import br.com.givailson.popularmoviesapp.views.MovieActivity;
+
+public class GrideMovieFragment extends Fragment implements AdapterView.OnItemClickListener, MovieService.MovieServiceCallBack {
+
+    private MovieGridAdapter movieGridAdapter;
+    private GridView moviesGridView;
+
+    private List<Movie> movies = new ArrayList<Movie>();
+
+    public GrideMovieFragment() {}
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_grid_movie, container, false);
+        moviesGridView = rootView.findViewById(R.id.gv_movies_list);
+        movieGridAdapter = new MovieGridAdapter(getActivity(), this.movies);
+        moviesGridView.setAdapter(movieGridAdapter);
+        moviesGridView.setOnItemClickListener(this);
+
+        new MovieService(this).execute("popular");
+
+        return rootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intentMovie = new Intent(this.getActivity(), MovieActivity.class);
+        intentMovie.putExtra("movie", this.movies.get(position));
+        startActivity(intentMovie);
+    }
+
+    @Override
+    public void onComplete(List<Movie> movies) {
+        this.movies = movies;
+        movieGridAdapter = new MovieGridAdapter(getActivity(), this.movies);
+        moviesGridView.setAdapter(movieGridAdapter);
+    }
+}
