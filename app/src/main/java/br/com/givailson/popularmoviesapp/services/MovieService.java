2aca0e2ebd3d371a48162860fa75609fce59e497
Givailson de Souza Neves <givasneves@MacBook-Pro-de-Givailson.local>
2018-10-04 08:26:18 -0300

    ajustes

--- app/src/main/java/br/com/givailson/popularmoviesapp/services/MovieService.java
+++ app/src/main/java/br/com/givailson/popularmoviesapp/services/MovieService.java
@@ -1,15 +1,36 @@
 package br.com.givailson.popularmoviesapp.services;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class MovieService extends AsyncTask<URL, Void, String> {
+
+    private final static String BASE_MOVIE_API_PATH = "";
     MovieServiceCallBack msb;
 
     MovieService(MovieServiceCallBack msb) {
         this.msb = msb;
+    }
+
+    public String listPopularMoviews() {
+        Uri uriBuilder = Uri.parse(BASE_MOVIE_API_PATH)
+                .buildUpon()
+                .build();
+
+        try {
+            URL url = new URL(uriBuilder.toString());
+            return this.doInBackground(url);
+        }catch (MalformedURLException me) {
+            me.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -17,13 +38,33 @@ public class MovieService extends AsyncTask<URL, Void, String> {
         URL servicePath = urls[0];
         String movieResults = null;
         try{
-            movieResults = NetworkUtils.getResponseFromHttpUrl(servicePath);
+            movieResults = this.getResponseFromHttpUrl(servicePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return movieResults;
     }
+
+    private String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
 
     @Override
     protected void onPostExecute(String s) {
