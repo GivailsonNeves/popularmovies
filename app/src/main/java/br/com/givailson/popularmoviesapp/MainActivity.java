a4153de6314c26b7a8b4e314e373ab44fe3a0d3a
Givailson de Souza Neves <givasneves@MacBook-Pro-de-Givailson.local>
2018-10-04 23:55:46 -0300

    montagem da tela de detalhe

--- app/src/main/java/br/com/givailson/popularmoviesapp/MainActivity.java
+++ app/src/main/java/br/com/givailson/popularmoviesapp/MainActivity.java
@@ -2,14 +2,22 @@ package br.com.givailson.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.givailson.popularmoviesapp.fragments.GrideMovieFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    GrideMovieFragment gmf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gmf = (GrideMovieFragment) getSupportFragmentManager().findFragmentById(R.id.movies_fragment);
+
     }
 
     @Override
@@ -17,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        gmf.orderBy(item.getItemId());
+        return false;
     }
 }
--- app/src/main/java/br/com/givailson/popularmoviesapp/views/MovieActivity.java
+++ app/src/main/java/br/com/givailson/popularmoviesapp/views/MovieActivity.java
@@ -3,12 +3,18 @@ package br.com.givailson.popularmoviesapp.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import br.com.givailson.popularmoviesapp.R;
 import br.com.givailson.popularmoviesapp.models.Movie;
+import br.com.givailson.popularmoviesapp.utils.Util;
 
 public class MovieActivity extends AppCompatActivity {
 
+    private final String basePathImage = "http://image.tmdb.org/t/p/w500/";
     private Movie movie;
 
     @Override
@@ -21,6 +27,17 @@ public class MovieActivity extends AppCompatActivity {
     }
 
     private void prepareMovie() {
-        Log.i("yago", this.movie.getTitle());
+        TextView tvTitle = findViewById(R.id.tv_title);
+        TextView tvReview = findViewById(R.id.tv_review);
+        TextView tvReleaseDate = findViewById(R.id.tv_release_date);
+        ImageView ivPoster = findViewById(R.id.iv_movie_poster);
+        StringBuilder urlImage = new StringBuilder(basePathImage)
+                .append(movie.getUriPhoto());
+
+        tvTitle.setText(movie.getTitle());
+        tvReleaseDate.setText(Util.datoToBRString(movie.getReleaseDate()));
+        tvReview.setText(movie.getSinopse());
+        Picasso.with(this).load(urlImage.toString()).into(ivPoster);
+
     }
 }
